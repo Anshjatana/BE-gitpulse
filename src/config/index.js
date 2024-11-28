723d6444350bae9b9c208b7e1a37b5fe4d7eec5c
Ansh Jatana <anshjatana1111@gmail.com>
2024-11-28 23:36:56 +0530

    Changed ai model to gemini now

--- src/config/index.js
+++ src/config/index.js
@@ -4,10 +4,10 @@ dotenv.config();
 
 export const config = {
   port: process.env.PORT || 8000,
-  openai: {
-    apiKey: process.env.OPENAI_API_KEY
-  },
   github: {
     token: process.env.GITHUB_TOKEN
-  }
+  },
+  google: {
+    apiKey: process.env.API_KEY
+  },
 };
